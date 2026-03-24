bdf247afb15ca3f3faaa6f5a9d5f4f00b94b3be5
alf <alfredjbphiri@gmail.com>
2026-03-25 00:07:12 +0200

    feat: Implement a dynamic tabbed interface with JavaScript functionality and comprehensive CSS styling, and set the initial active tab in HTML.

--- js/tabs.js
+++ js/tabs.js
@@ -8,33 +8,41 @@ const aboutContainers = document.querySelectorAll(".about-container");
 const servicesContainer = document.querySelector(".services-container");
 const contactsContainer = document.querySelector(".contacts-container");
 
-// Hide all tab content
+const tabs = document.querySelectorAll(".tab-header div");
+
+// Hide all tab content and remove active classes
 function hideAll() {
     homeContainer.style.display = "none";
     aboutContainers.forEach(container => container.style.display = "none");
     servicesContainer.style.display = "none";
     contactsContainer.style.display = "none";
+    
+    tabs.forEach(tab => tab.classList.remove("active"));
 }
 
 // Show content based on tab clicked
 home.addEventListener("click", () => {
     hideAll();
     homeContainer.style.display = "block";
+    home.classList.add("active");
 });
 
 about.addEventListener("click", () => {
     hideAll();
     aboutContainers.forEach(container => container.style.display = "block");
+    about.classList.add("active");
 });
 
 services.addEventListener("click", () => {
     hideAll();
     servicesContainer.style.display = "block";
+    services.classList.add("active");
 });
 
 contacts.addEventListener("click", () => {
     hideAll();
     contactsContainer.style.display = "block";
+    contacts.classList.add("active");
 });
 
 // Show home tab by default
